57ec29db755ddef70130f651ef622cf0e544f57a
Zichen Wang <wangzichen530@hotmail.com>
2016-12-16 13:09:34 -0600

    checkpoint 8 assignment complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -62,4 +62,14 @@ class AddressBook
    return nil
  end
 
+ def iterative_search (name)
+   entries.each do |entry|
+     if entry.name == name
+       return entry
+     end
+   end
+
+   return nil
+ end
+
 end
